cd912fc9be6a9ecf7616308e796e5feecfea98e5
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-11-20 09:51:46 -0700

    Remove old path

--- admixture/2-1_admixture_cv.sh
+++ admixture/2-1_admixture_cv.sh
@@ -7,8 +7,7 @@
 
 while getopts o: opt; do
    case "${opt}" in
-      o) OUT=${OPTARG};;  # path to out dir, Immunogenetics_T1D/genetics/teddy_r01/ancestry_estimation
-        # new out dir Immunogenetics_T1D/genetics/teddy_r01/admixture, make subfolder ancestry_estimation
+      o) OUT=${OPTARG};;  # path to out dir
       \?) echo "Invalid option -$OPTARG" >&2
       exit 1;;
    esac
