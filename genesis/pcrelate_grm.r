bd0d901311be26b40863e2848e653a4b42f74e93
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-12-01 16:27:17 -0700

    Add small sample flag, fix typo

--- genesis/pcrelate_grm.r
+++ genesis/pcrelate_grm.r
@@ -5,6 +5,7 @@ args <- commandArgs(trailingOnly=TRUE)
 plink_prefix <- args[1]  # path to file with name but without extension
 pruned_list <- args[2]
 num_pcs <- as.numeric(args[3])
+small_sample <- args[4]  # should be "yes" if < 5000, otherwise "no" 
 
 library(SNPRelate)
 library(GENESIS)
@@ -14,10 +15,9 @@ geno <- GdsGenotypeReader(paste0(plink_prefix, ".gds"))
 genoData <- GenotypeData(geno)
 pruned <- read.table(pruned_list)[,1]
 mypcair <- readRDS(paste0(plink_prefix, "_pcair.rds"))
+small_sample_flag <- small_sample == "yes"
 
 # PC-Relate
-    #TODO: revisit adding more iterations?
-    #TODO: add option for small sample correction on/off!
 genoData <- GenotypeBlockIterator(genoData, snpInclude=pruned)
 
 print(paste0("Running PCRelate with ", num_pcs, " PCs."))
@@ -27,7 +27,7 @@ mypcrelate <- pcrelate(genoData,
                        BPPARAM = BiocParallel::MulticoreParam(4),
                        ibd.probs = TRUE,
                        sample.block.size = 5000, # default
-                       small.samp.correct = TRUE, # 5000 < 7036 => FALSE
+                       small.samp.correct = small_sample_flag,
                        maf.thresh = 0.01) # default
 close(genoData)
 saveRDS(mypcrelate, paste0(plink_prefix, "_pcrelate.rds"))
@@ -42,7 +42,7 @@ e_vals <- eigen(grm, symmetric = T, only.values = T)
 e_vals_neg <- sum(e_vals$values < 0)
 
 if (e_vals_neg > 0) {
-    print("There are ", e_vals_neg, " negative eigenvalues:")
+    print(paste0("There are ", e_vals_neg, " negative eigenvalues:"))
     print(e_vals$values[e_vals$values < 0])
     print("These will be corrected with Matrix::nearPD, but it should")
     print("be confirmed that it seems reasonble to do that.")
